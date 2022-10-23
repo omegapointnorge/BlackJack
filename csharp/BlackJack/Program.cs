9d731a466772c21ad68ac59df4e16531c90d3fef
Martin <martin.johansen99@hotmail.com>
2022-10-23 10:42:00 +0200

    Forgot to change back bust check

--- csharp/BlackJack/Program.cs
+++ csharp/BlackJack/Program.cs
@@ -22,7 +22,7 @@ namespace BlackJack
                     hand.Add(card);
                     var total = hand.Sum(x => Math.Min(x.Rank, 10));
 
-                    if (total > 221)
+                    if (total > 21)
                     {
                         Console.WriteLine($"The sum of your hand exceedes 21, which means you've lost this round");
                         break;
